72658fb57452fd7d068561605ee37dc2a8b680c0
m <m@bitsnbites.eu>
2018-08-17 12:55:42 +0200

    Quit when ESC is pressed

--- game1.py
+++ game1.py
@@ -10,21 +10,28 @@ def main():
     icon = pygame.image.load("icon_128x128.png")
     pygame.display.set_icon(icon)
     pygame.display.set_caption("My game")
-     
+
     # Create a surface on screen that has the size of 1024 x 768.
     screen = pygame.display.set_mode((1024, 768))
      
     # Main loop.
     running = True
     while running:
-        # Event handling: get all events from the eventqueue.
+        # Event handling: Get all events from the eventqueue.
         for event in pygame.event.get():
-            # Only do something if the event is of type QUIT.
-            if event.type == pygame.QUIT:
+            # Should we quit?
+            if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
                 # Stop the main loop.
                 running = False
-     
-     
+
+        # TODO: Do something :-)
+
+
+    # Stop the program.
+    pygame.quit()
+    sys.exit()
+
+
 # Run the main function only if this module is executed as the main script.
 if __name__=="__main__":
     main()
